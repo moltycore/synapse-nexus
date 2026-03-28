7e05d041c9da82a899f82e1a14c8c00428c3b2a2
moltycore <idristan97@gmail.com>
2026-03-28 20:56:35 +0300

    Update SynapseInput.tsx

--- src/components/SynapseInput.tsx
+++ src/components/SynapseInput.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, KeyboardEvent } from "react";
+"import { useState, useRef, useEffect, KeyboardEvent } from "react";
 import { CornerDownLeft } from "lucide-react";
 
 interface SynapseInputProps {
