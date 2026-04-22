4b07180b9b35b1708c50c841b39977f9c1096483
moltycore <idristan97@gmail.com>
2026-04-22 23:09:27 +0300

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 export const API_URL = import.meta.env.VITE_API_URL as string;
 
-export const APP_VERSION = "V1.2.19";
+export const APP_VERSION = "V1.2.29";
 
 export const STREAM_TIMEOUT_MS = 90_000;
